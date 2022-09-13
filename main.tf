e58a8d43cfb15c4efb680d4fecf7527d32651b3f
Chandruhere4u <Chandruhere4u@gmail.com>
2022-09-13 13:13:49 -0400

    Adding main tf and git actions

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "TestInsanceAssumeRole" {
+    ami = "ami-01147599e39cb5802"
+    instance_type = "t2.micro"
+    tags = {
+      name="TestInstance3"
+    }
+  }
