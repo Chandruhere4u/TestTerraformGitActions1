47195053ad145d2bf6ce960577361bdba1e466ef
Chandruhere4u <Chandruhere4u@gmail.com>
2022-09-13 13:31:58 -0400

    Trying again

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "ChandruTestOrg"
+
+    workspaces {
+      name = "TestTerraformGitActions1"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
